53d54253e42ce47be76153b55fd2c81208350d74
zxkang <zongxue.kzx@vivo.com>
2019-04-03 15:24:25 +0800

    增加简繁切换event

---
+++ app/src/main/java/com/example/newbiechen/ireader/event/SimTraConvertEvent.java
@@ -0,0 +1,10 @@
+package com.example.newbiechen.ireader.event;
+
+/**
+ * Created by zxkang on 2019/4/3 上午10:31.
+ */
+
+public class SimTraConvertEvent {
+    public SimTraConvertEvent() {
+    }
+}
